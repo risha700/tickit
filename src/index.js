99be72ed8b86df5f0e4fc858506b669ed63c4010
R$ <Rs@Rs-MacBook-Pro.local>
2018-01-03 13:44:50 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(
+
+
+
+	<div >
+
+		<h1>Hello Richie</h1>
+
+	</div>
+
+
+
+
+
+	, document.getElementById('root')
+
+
+
+	)
